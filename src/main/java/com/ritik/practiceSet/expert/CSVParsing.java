e0e01ea6ef21e0447a9b405c8a7be7d5b5c8bc30
Ritik Ranjan <ritikranjan999@gmail.com>
2025-08-25 15:37:11 +0530

    Add CSV file and parser for NCRB crime data
    Introduced `NCRB_Table.csv` containing crime statistics by state/UT from 2020 to 2022. Added `CSVParsing.java` to parse the file and map the data to the `CrimeData` class for processing and display.

---
+++ src/main/java/com/ritik/practiceSet/expert/CSVParsing.java
@@ -0,0 +1,100 @@
+package com.ritik.practiceSet.expert;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+class CrimeData {
+    private int SlNo;
+    private String State_UT;
+    private float n2020;
+    private float n2021;
+    private float n2022;
+    private float midYearProjectedPopulation;
+    private float rateOfCognizableCrimes;
+    private float chargesheetRate;
+
+    public CrimeData(int slNo, String state_UT, float n2020, float n2021, float n2022,
+                     float midYearProjectedPopulation, float rateOfCognizableCrimes, float chargesheetRate) {
+
+        SlNo = slNo;
+        State_UT = state_UT;
+        this.n2020 = n2020;
+        this.n2021 = n2021;
+        this.n2022 = n2022;
+        this.midYearProjectedPopulation = midYearProjectedPopulation;
+        this.rateOfCognizableCrimes = rateOfCognizableCrimes;
+        this.chargesheetRate = chargesheetRate;
+    }
+
+    @Override
+    public String toString() {
+        return "CrimeData{" +
+                "SlNo=" + SlNo +
+                ", State_UT='" + State_UT + '\'' +
+                ", n2020=" + n2020 +
+                ", n2021=" + n2021 +
+                ", n2022=" + n2022 +
+                ", midYearProjectedPopulation=" + midYearProjectedPopulation +
+                ", rateOfCognizableCrimes=" + rateOfCognizableCrimes +
+                ", chargesheetRate=" + chargesheetRate +
+                '}';
+    }
+}
+
+public class CSVParsing {
+    public static void main(String[] args) throws IOException {
+        String csvFilePath = "src/main/java/com/ritik/practiceSet/expert/csv/NCRB_Table.csv";
+        File file = new File(csvFilePath);
+
+        if (!file.exists()) {
+            System.out.println("File does not exist");
+            return;
+        }
+
+        List<CrimeData> list;
+        try (Stream<String> stream = Files.lines(file.toPath())) {
+            list = stream.skip(1)
+                    .map(line -> line.split(","))
+                    .map(stateData ->
+                            new CrimeData(
+                                    Integer.parseInt(stateData[0]),
+                                    stateData[1],
+                                    Float.parseFloat(stateData[2]),
+                                    Float.parseFloat(stateData[3]),
+                                    Float.parseFloat(stateData[4]),
+                                    Float.parseFloat(stateData[5]),
+                                    Float.parseFloat(stateData[6]),
+                                    Float.parseFloat(stateData[7])
+                            )).toList();
+        }
+        
+        if (list.isEmpty()) {
+            System.out.println("No data found");
+            return;
+        }
+
+        for (CrimeData crimeData : list) {
+            System.out.println(crimeData);
+        }
+
+
+    }
+
+    void readFile(File file) throws FileNotFoundException {
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                for (String value : values) {
+                    System.out.print(value.trim() + " ");
+                }
+                System.out.println();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
